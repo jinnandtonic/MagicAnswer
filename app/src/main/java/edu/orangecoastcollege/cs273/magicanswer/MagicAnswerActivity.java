dcda00ff724cffcfcecb0b7879aebb17637da5f1
rmillett <rmillett@student.cccd.edu>
2017-10-31 16:30:02 -0700

    final commit

--- app/src/main/java/edu/orangecoastcollege/cs273/magicanswer/MagicAnswerActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/magicanswer/MagicAnswerActivity.java
@@ -1,5 +1,8 @@
 package edu.orangecoastcollege.cs273.magicanswer;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -14,6 +17,10 @@ public class MagicAnswerActivity extends AppCompatActivity {
 
     MagicAnswer magicAnswer;
     private TextView answerTextView;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
+    private ShakeDetector mShakeDetector; // reference to ShakeDetector
 
     /**
      * Creates an instance of <code>MagicAnswerActivity</code> in the view
@@ -28,10 +35,43 @@ public class MagicAnswerActivity extends AppCompatActivity {
         answerTextView = (TextView) findViewById(R.id.answerTextView);
 
         // TASK 2: CREATE A NEW MAGIC ANSWER OBJECT
+        magicAnswer = new MagicAnswer(this);
 
         // TASK 3: REGISTER THE SENSOR MANAGER AND SETUP THE SHAKE DETECTION
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
+        mShakeDetector = new ShakeDetector(new ShakeDetector.OnShakeListener() {
+            @Override
+            public void onShake() {
+                displayMagicAnswer();
+            }
+        });
     }
 
+    /**
+     * Displays a random answer after a shake event has occurred
+     */
+    public void displayMagicAnswer() {
+        String randomAnswer = magicAnswer.getRandomAnswer();
+        answerTextView.setText(randomAnswer);
+    }
 
+    /**
+     * Overrides the onResume() method to register the shake detector
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    /**
+     * Overrides the onStop() method to unregister the shake detector
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(mShakeDetector, mAccelerometer);
+    }
 }
