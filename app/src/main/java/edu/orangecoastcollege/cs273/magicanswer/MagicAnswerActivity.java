373bbbae7e0563dd1d1aa867f1eec26713982349
Millett <rmillett@student.cccd.edu>
2017-10-31 14:34:18 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/magicanswer/MagicAnswerActivity.java
@@ -0,0 +1,37 @@
+package edu.orangecoastcollege.cs273.magicanswer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * <code>MagicAnswerActivity</code> uses the built-in accelerator sensor to pseudo-randomly generate
+ * an answer to a user's question, much like a Magic 8 ball.
+ *
+ * When user "shakes" the device (using the accelerator sensor) a pseudo-random answer is displayed
+ */
+public class MagicAnswerActivity extends AppCompatActivity {
+
+    MagicAnswer magicAnswer;
+    private TextView answerTextView;
+
+    /**
+     * Creates an instance of <code>MagicAnswerActivity</code> in the view
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_magic_answer);
+
+        // TASK 1: SET THE REFERENCES TO THE LAYOUT ELEMENTS
+        answerTextView = (TextView) findViewById(R.id.answerTextView);
+
+        // TASK 2: CREATE A NEW MAGIC ANSWER OBJECT
+
+        // TASK 3: REGISTER THE SENSOR MANAGER AND SETUP THE SHAKE DETECTION
+
+    }
+
+
+}
